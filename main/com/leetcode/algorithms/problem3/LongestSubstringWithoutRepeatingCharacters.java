a2875fbe55cb79bc3a25365cc0bd22737a171a54
tianjiande@gmail.com <tianjiande@gmail.com>
2017-10-16 00:05:24 +0800

    Longest Substring Without Repeating Characters，完成

---
+++ main/com/leetcode/algorithms/problem3/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,29 @@
+package com.leetcode.algorithms.problem3;
+
+import java.util.*;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        int start = 0;
+        HashMap<Character, Integer> keys = new HashMap<>();
+        int current = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (keys.containsKey(c)) {
+                if (current > max) {
+                    max = current;
+                }
+                if (start <= keys.get(c)) {
+                    start = keys.get(c) + 1;
+                    current = i - start;
+                }
+            }
+            current++;
+            keys.put(c, i);
+        }
+        return max < current ? current : max;
+    }
+}
