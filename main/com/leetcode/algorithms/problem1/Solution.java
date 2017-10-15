eca9940d36056983c6a1c5fdfbf5823d292a744e
tianjiande@gmail.com <tianjiande@gmail.com>
2017-10-15 18:53:34 +0800

    format code

--- main/com/leetcode/algorithms/problem1/Solution.java
+++ main/com/leetcode/algorithms/problem1/Solution.java
@@ -4,14 +4,13 @@ import java.util.HashMap;
 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> set = new HashMap<>();
+        HashMap<Integer, Integer> hashTableForInput = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
-
-                     if(set.containsKey(target-num)){
-                         return new int[]{set.get(target-num),i};
-                     }
-            set.put(num,i);
+            if (hashTableForInput.containsKey(target - num)) {
+                return new int[]{hashTableForInput.get(target - num), i};
+            }
+            hashTableForInput.put(num, i);
         }
         return null;
     }
