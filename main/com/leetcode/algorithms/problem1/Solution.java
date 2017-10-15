d37e8cfd60cbb995ec442673e0235f58167b7d26
tianjiande@gmail.com <tianjiande@gmail.com>
2017-10-15 17:42:09 +0800

    twoSum

---
+++ main/com/leetcode/algorithms/problem1/Solution.java
@@ -0,0 +1,11 @@
+package com.leetcode.algorithms.problem1;
+
+import java.util.HashSet;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashSet<Integer> set = new HashSet<>();
+
+        return null;
+    }
+}
